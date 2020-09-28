2329d1e8291db7ded8ff6a5ad6a4f42e0511bf34
Galelya <s.galina97@mail.ru>
2020-09-28 11:15:40 +0300

    finished version

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ const cssnano = require("cssnano");
 const postcss = require("gulp-postcss");
 const sass = require("gulp-sass");
 const gls = require('gulp-live-server');
+const urlAdjuster   = require('gulp-css-url-adjuster');
 
 // CSS task
 // function scss() {
@@ -30,14 +31,15 @@ gulp.task('watch', function() {
     gulp.watch('./main/scss/**/*.scss', function(file) {
 
         del(["./main/css/**"]);
-        const res = gulp
+        return gulp
             .src("./main/scss/style.scss")
             .pipe(sass())
+            .pipe(urlAdjuster({
+                replace:  ['../../assets','../assets'],
+            }))
             .pipe(postcss([autoprefixer(), cssnano()]))
             .pipe(gulp.dest("./main/css/"))
             .pipe(server.notify.apply(server, [file]));
-
-        return res;
     });
 });
 
