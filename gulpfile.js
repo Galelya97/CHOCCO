bb82487135238a7b2470f34264f7b7bbdcfe9ab7
Galelya <s.galina97@mail.ru>
2020-10-06 14:07:06 +0300

    change flex

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
-const gulp = require('gulp');
-const del = require('del');
+const gulp = require("gulp");
+const del = require("del");
 const autoprefixer = require("autoprefixer");
 const cssnano = require("cssnano");
 const postcss = require("gulp-postcss");
 const sass = require("gulp-sass");
-const gls = require('gulp-live-server');
-const urlAdjuster   = require('gulp-css-url-adjuster');
+const gls = require("gulp-live-server");
+const urlAdjuster = require("gulp-css-url-adjuster");
 
 // CSS task
 // function scss() {
@@ -25,22 +25,27 @@ const urlAdjuster   = require('gulp-css-url-adjuster');
 
 // const build = gulp.series(clean, gulp.parallel(css));
 
-gulp.task('watch', function() {
-    const server = gls.static('main', 5000);
-    server.start();
-    gulp.watch('./main/scss/**/*.scss', function(file) {
+function sassFn() {
+  del(["./main/css/**"]);
+  return gulp
+    .src("./main/scss/style.scss")
+    .pipe(sass())
+    .pipe(
+      urlAdjuster({
+        replace: ["../../assets", "../assets"],
+      })
+    )
+    .pipe(postcss([autoprefixer(), cssnano()]))
+    .pipe(gulp.dest("./main/css/"));
+}
 
-        del(["./main/css/**"]);
-        return gulp
-            .src("./main/scss/style.scss")
-            .pipe(sass())
-            .pipe(urlAdjuster({
-                replace:  ['../../assets','../assets'],
-            }))
-            .pipe(postcss([autoprefixer(), cssnano()]))
-            .pipe(gulp.dest("./main/css/"))
-            .pipe(server.notify.apply(server, [file]));
-    });
+gulp.task("watch", function () {
+  const server = gls.static("main", 5000);
+  sassFn();
+  server.start();
+  gulp.watch("./main/scss/**/*.scss", function (file) {
+    return sassFn().pipe(server.notify.apply(server, [file]));
+  });
 });
 
 // exports.build = build;
